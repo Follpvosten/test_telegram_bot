39222b812877156cfd52d06713e914095320e72c
Follpvosten <wolfi@karpador.xyz>
2018-03-12 14:16:57 +0100

    Removed bot token from code (that wasn't my brightest day)

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,20 @@ use tokio_core::reactor::Core;
 use futures::stream::Stream;
 use futures::Future;
 use failure::Error;
+use std::env;
 
 use telebot::functions::*;
 
 fn main() {
     println!("Hello, world!");
 
-    // Telebot stuff, yay!
+    // TELEGRAM_BOT_TOKEN has to be set, of course
+    let bot_token = env::var("TELEGRAM_BOT_KEY");
+    if bot_token.is_err() {
+        panic!("Please set the environment variable TELEGRAM_BOT_KEY");
+    }
     let mut lp = Core::new().unwrap();
-    let my_bot = RcBot::new(lp.handle(), "563680830:AAHWVHApuosu9wwZN2XPEXQkLl5tYe6shBQ")
-        .update_interval(200);
+    let my_bot = RcBot::new(lp.handle(), &bot_token.unwrap()).update_interval(200);
 
     let handle = my_bot.new_cmd("/start").and_then(handle_start);
 
